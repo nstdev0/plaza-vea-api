729087fb20473cfd13f0ac75473bf2b5e0e22002
nstdev0 <nst.dev0@gmail.com>
2026-01-14 13:52:09 -0500

    feat: enhance API security and performance
    - Added helmet for security headers
    - Implemented CORS with specific settings
    - Introduced rate limiting to prevent abuse
    - Created a health check endpoint
    - Improved error handling for undefined routes

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,49 @@
 import express, { type Application } from "express";
+import helmet from "helmet";
+import cors from "cors";
 import morgan from "morgan";
 import { errorHandler } from "./presentation/middlewares/errorHandler";
 
 import productRoutes from "./routes/products";
+import rateLimit from "express-rate-limit";
 
 const app: Application = express();
 
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // Ventana de 15 minutos
+  max: 100, // Máximo 100 peticiones por IP cada 15 min
+  standardHeaders: true, // Devuelve headers `RateLimit-*` (Estándar moderno)
+  legacyHeaders: false, // Deshabilita headers `X-RateLimit-*` (Viejos)
+  message: {
+    status: 429,
+    error: "Too many requests",
+    message:
+      "Has excedido el límite de peticiones. Intenta de nuevo en 15 minutos.",
+  },
+});
+
+app.use(helmet());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
-// // Setup dependencies:
-
-// // Infrastructure
-// const productRepository = new ProductRepository();
-// const vtexService = new VtexService();
-
-// // Use-cases
-// const getProductBySkuIdUseCase = new GetProductBySkuIdUseCase(
-//   productRepository,
-//   vtexService
-// );
-
-// // Controllers
-// const productController = new ProductController(getProductBySkuIdUseCase);
-
 // Routes
+app.use("/api", apiLimiter);
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "OK", timestamp: new Date() });
+});
+
 app.use("/api/products", productRoutes);
 
-app.use("/api", (req, res) => {
-  res.send("API is running");
+app.use("/api/", (req, res) => {
+  res.status(404).json({ error: "Endpoint not found" });
 });
 
 // Middleware
