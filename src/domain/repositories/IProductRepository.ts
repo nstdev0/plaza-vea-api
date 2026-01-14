78a5a61e649631f628f3a0e5780c67f445fa486e
nstdev0 <nst.dev0@gmail.com>
2026-01-14 14:03:26 -0500

    feat: add create method to IProductRepository for product creation

--- src/domain/repositories/IProductRepository.ts
+++ src/domain/repositories/IProductRepository.ts
@@ -1,4 +1,5 @@
 import type { Product } from "@domain/entities/Product";
+import type { ProductCreateInput } from "../../../generated/prisma/models";
 import type {
   IPageableRequest,
   IPageableResult,
@@ -8,4 +9,5 @@ export interface IProductRepository {
   getAll(filters: IPageableRequest): Promise<IPageableResult<Product>>;
   findBySkuId(skuId: string): Promise<Product | null>;
   findByEan(ean: string): Promise<Product | null>;
+  create(data: ProductCreateInput): Promise<Product>;
 }
