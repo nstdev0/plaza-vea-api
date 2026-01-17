e39f9f6603d303e2f922a26376e3f344f9208c4b
nstdev0 <nst.dev0@gmail.com>
2026-01-17 16:06:38 -0500

    refactor: Change IProductRepository create method parameter from ProductCreateInput to Product.

--- src/domain/repositories/IProductRepository.ts
+++ src/domain/repositories/IProductRepository.ts
@@ -2,13 +2,12 @@ import type {
   IPageableRequest,
   IPageableResult,
 } from "../../application/common/pagination.js";
-import type { ProductCreateInput } from "../../generated/prisma/models.js";
 import type { Product } from "../entities/Product.js";
 
 export interface IProductRepository {
   getAll(filters: IPageableRequest): Promise<IPageableResult<Product>>;
   findBySkuId(skuId: string): Promise<Product | null>;
   findByEan(ean: string): Promise<Product | null>;
-  create(data: ProductCreateInput): Promise<Product>;
+  create(data: Product): Promise<Product>;
   deleteAll(): Promise<boolean>;
 }
