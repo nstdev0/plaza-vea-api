a6e57bc18c103f6efd5866e37e05210b8d44691e
nstdev0 <nst.dev0@gmail.com>
2026-01-21 14:02:10 -0500

    "chore: start server only in non-production environments"

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,8 @@ import { AppConfig } from "./config/config.js";
 
 const PORT = AppConfig.PORT;
 
-const server = app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-
-// ⏱️ Timeout global del servidor (Node HTTP)
-server.setTimeout(10 * 60_000); // 10 minutos
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
